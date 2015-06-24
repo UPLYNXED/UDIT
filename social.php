4d113b366612cc9c7b5d5f4fc285f296c1c6ac24
Levi Pluim Mentz <l.pluimmentz@student.fontys.nl>
2015-06-24 11:24:24 +0200

    new file, social.php

---
+++ social.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<?php include 'includes/head.php'; ?>
+	<body>
+		<div class="container">
+			<div class="loginScreen">
+				<?php
+					$pageTitle = 'Register';
+					$menuVisible = false;
+					include 'includes/nav.php';
+				?>
+				<h2 class="loginTitle">Udit</h2>
+				<form>
+					<input type="text" name="name" placeholder="first name">
+					<input type="text" name="lastname" placeholder="last name">
+					<input type="text" name="email" placeholder="email">
+					<input type="password" name="password" placeholder="password">
+					<input type="submit" name="btnAction" value="register">
+				</form>
+				<div class="noAccount">
+					<span>
+						Already have an account?<br>
+						<a href="index.php">SIGN IN</a>
+					</span>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
